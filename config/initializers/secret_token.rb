933acfc7afcd2189bd249db69428b2d7ebc976ff
Kvapa <jirikvapil@gmail.com>
2012-12-31 19:00:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObecDetrichov::Application.config.secret_token = '126d51e5f4a30772b27de6eb4b653ebd8c8214059944894ae662a6593362933a3ccb2fc7d1d693859fac7b2678b4ec9733251bdbb047e1c708e0a6730b0e2551'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ObecDetrichov::Application.config.session_store :cookie_store, key: '_obec_detrichov_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ObecDetrichov::Application.config.session_store :active_record_store
